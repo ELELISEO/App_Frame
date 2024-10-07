debf6892f0563c31cd487ee0f442dd8bc7f83fe2
ELELISEO <jcolunga09@alumnos.uaq.mx>
2024-10-07 13:20:42 -0600

    add. page initial

---
+++ src/components/sendSpace/SendSpace.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const SendSpace = () => {
+  return (
+    <>
+      <div className='bg-white h-[30rem] w-[65rem] m-10'>
+        SendSpace
+      </div>
+    </>  
+  )
+}
+
+export default SendSpace
--- src/pages/PageInitial.jsx
+++ src/pages/PageInitial.jsx
@@ -1,8 +1,23 @@
 import React from 'react'
+import SendSpace from '../components/sendSpace/SendSpace'
+import Search from '../components/search/Search'
 
 const PageInitial = () => {
   return (
-    <div>PageInitial</div>
+    <>
+    <main className='h-screen w-screen'>
+      <section className='flex h-full w-full'>
+      <div className='bg-black w-96 h-full text-white text-3xl'>
+        barra de tareas
+      </div>
+      <div className='h-full w-full bg-[#d9d9d9] flex justify-start items-end flex-col'>
+        <Search/>
+        <SendSpace/>
+        queso
+      </div>
+    </section>
+    </main>
+    </>
   )
 }
 
