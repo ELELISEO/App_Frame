c58f77e62639161d3360492a0022a74910110c81
Daniela Camacho Gomez <camachodaniela143@gmail.com>
2024-10-14 14:09:19 -0600

    add: Login

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 import React from 'react'
 import { BrowserRouter } from "react-router-dom";
 import { Routes, Route } from "react-router-dom"
-import Home from './pages/Home';
 import PageInitial from './pages/PageInitial';
+import { Login } from './pages/Login';
 
 
 const App = () => {
   return (
     <BrowserRouter>
     <Routes>
-        <Route path="/" element={ <Home/> } />
+        <Route path="login" element={ <Login/> } />
         <Route path="PageI" element={ <PageInitial /> } />
     </Routes>
     </BrowserRouter>
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,19 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        'color1': '#919191',
+        'color2': '#ADADAD',
+        'color3': '#C8C8C8',
+        'color4': '#E4E4E4',
+        'color5': '#FFFFFF',
+        'color6': '#D9D9D9',
+        'color7' :'#EDEDED',
+        'color8': '#419AFF',
+      },
+    },
+
   },
   plugins: [],
 }
